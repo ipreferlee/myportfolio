d15757c6e939fa66a9a93627eae806324a9e8cf4
ipreferlee <lrochoada@gbox.ncf.edu.ph>
2024-12-03 22:27:10 +0800

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -5,6 +5,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import './App.jsx';
+
 
 const About = () => {
   return (
