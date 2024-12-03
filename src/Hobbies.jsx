8e030b0a24a5aa6409670f833b5bd36d09610067
ipreferlee <lrochoada@gbox.ncf.edu.ph>
2024-12-03 21:39:06 +0800

    Update Hobbies.jsx

--- src/Hobbies.jsx
+++ src/Hobbies.jsx
@@ -26,7 +26,7 @@ const Hobbies = () => {
               <Nav.Link as={Link} to="/">Home</Nav.Link>
               <Nav.Link as={Link} to="/About">About</Nav.Link>
               <Nav.Link as={Link} to="/Education">Education</Nav.Link>
-              <Nav.Link as={Link} to="/Hobbies">Hobbies</Nav.Link>
+              <Nav.Link as={Link} to="/">Hobbies</Nav.Link>
               <Nav.Link as={Link} to="/Contact">Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
