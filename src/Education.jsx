e2cc4233a86bc6ab218b49daeceec2ed207b033f
ipreferlee <lrochoada@gbox.ncf.edu.ph>
2024-12-03 08:27:37 +0800

    Update Education.jsx

--- src/Education.jsx
+++ src/Education.jsx
@@ -8,7 +8,7 @@ import myImage from './assets/Person-Assets.png';
 import ncfLogo from './assets/NCF-LOGO.png';
 import angelesLogo from './assets/angeles.jpg';
 import systemPlus from './assets/systemplus.jpg';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 const Education = () => {
