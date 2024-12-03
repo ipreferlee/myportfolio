e824c15988335e4ef51a4a36fd15175f9d2c537a
ipreferlee <lrochoada@gbox.ncf.edu.ph>
2024-12-03 08:27:58 +0800

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import myImage from './assets/myphoto.jpg';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 const Home = () => {
