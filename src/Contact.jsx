c2b8a8848b3db714ea188a94c51529cf9dcc86e8
ipreferlee <lrochoada@gbox.ncf.edu.ph>
2024-12-03 08:26:19 +0800

    Update Contact.jsx

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 const Contact = () => {
   // State for form inputs
