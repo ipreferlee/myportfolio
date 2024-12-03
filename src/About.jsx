cb4e85865d39449306978de8fe48d8f9819ca955
ipreferlee <lrochoada@gbox.ncf.edu.ph>
2024-12-03 21:27:47 +0800

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -4,7 +4,14 @@ import myImage from './assets/Person-Assets.png';  // Assuming you want to use t
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Home';
+import About from './About';
+import Education from './Education';
+import Hobbies from './Hobbies';
+import Contact from './Contact';
+import './App.css';
 
 const About = () => {
   return (
