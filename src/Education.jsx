1b9ddc0a058ef2288433cf7105d5a52dbf314e7f
ipreferlee <lrochoada@gbox.ncf.edu.ph>
2024-12-03 21:36:10 +0800

    Update Education.jsx

--- src/Education.jsx
+++ src/Education.jsx
@@ -12,7 +12,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Home';
 import About from './About';
-import Education from './Education';
 import Hobbies from './Hobbies';
 import Contact from './Contact';
 import './App.css';
